14cf4e36d5fe69133e42bfce38ec3ad11fc1d01e
richard <richard_lucas.om12@gmail.com>
2020-11-12 15:32:09 -0300

    calculadora-de-medias

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nova_pasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite a primeira nota");
+            int nota1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite a segunda nota");
+            int nota2 = int.Parse(Console.ReadLine());
+            Console.WriteLine( "digite a terceira nota");
+            int nota3 = int.Parse(Console.ReadLine());
+            int media = (nota1 +nota2 +nota3)/3;
+            Console.WriteLine("sua media é = " + media );
+            
+            if
+                (media>5)
+            {
+                Console.WriteLine("Aprovado, Parabéns");
+            }
+            else
+             {
+                Console.WriteLine("Reporvado, Estude mais"); 
+            }
+        }
+    }
+}
